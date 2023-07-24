65144eab8ad3f6b94ee44db3b5dc1290ebd9bf60
OXKernel <OpenSource@rdoss.com>
2023-07-24 13:12:10 -0400

    Update prun.sh

--- prun.sh
+++ prun.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
+# Copyright (C) 2023. Roger Doss. All Rights Reserved.
+# Provided under MIT License.
 rm -f .randPrime.search.done
 perl randPrime_thread.pl
 perl randPrime_thread.pl
